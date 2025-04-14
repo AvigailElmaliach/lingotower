0ce9f5fcee974f8ae50f09b6da7477de84414e78
YoelRabinovich <joel2012r@gmail.com>
2025-04-14 15:38:31 +0300

    admin delete and update

--- LingotowerClient/src/main/java/com/lingotower/dto/admin/AdminResponseDTO.java
+++ LingotowerClient/src/main/java/com/lingotower/dto/admin/AdminResponseDTO.java
@@ -1,13 +1,50 @@
 package com.lingotower.dto.admin;
+
 import com.lingotower.model.Role;
+
 public class AdminResponseDTO {
-    private String username;
-    private Role role;
-    // Public constructor, Getters needed by Jackson/RestTemplate
-    public AdminResponseDTO() {}
-    public AdminResponseDTO(String u, Role r) { username = u; role = r; }
-    public String getUsername() { return username; }
-    public Role getRole() { return role; }
-    public void setUsername(String u) {username=u;} // Setter might be needed
-    public void setRole(Role r) {role=r;}       // Setter might be needed
+
+	private String username;
+	private String email;
+	private Long id;
+	private Role role;
+
+	public AdminResponseDTO(Long id, String username, String email, Role role) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.role = role;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
 }
--- LingotowerClient/src/main/java/com/lingotower/dto/admin/AdminUpdateDTO.java
+++ LingotowerClient/src/main/java/com/lingotower/dto/admin/AdminUpdateDTO.java
@@ -4,31 +4,39 @@ import com.lingotower.model.Role;
 
 public class AdminUpdateDTO {
 	private String username;
-	private String password; // Optional
 	private Role role;
+	private String password; // אופציונלי: לשינוי סיסמה
+	private String email;
 
-	// Public constructor (optional), Getters, Setters needed
 	public String getUsername() {
 		return username;
 	}
 
-	public void setUsername(String u) {
-		username = u;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String p) {
-		password = p;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 	public Role getRole() {
 		return role;
 	}
 
-	public void setRole(Role r) {
-		role = r;
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
